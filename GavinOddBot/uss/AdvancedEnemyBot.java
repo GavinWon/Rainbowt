3601bc61e675eb4a29e33dff8da33fec75a7e0f3
Gavin <gavinmwong@gmail.com>
2020-01-16 20:54:00 -0800

    added my odd bot

---
+++ GavinOddBot/uss/AdvancedEnemyBot.java
@@ -0,0 +1,116 @@
+package uss;
+
+import robocode.*;
+
+/**
+ * Record the advanced state of an enemy bot.
+ * 
+ * @author Josh
+ * @version 5/9/18
+ * 
+ * @author Period - 6
+ * @author Assignment - AdvancedEnemyBot
+ * 
+ * @author Sources - None
+ */
+public class AdvancedEnemyBot extends EnemyBot
+{
+    private double x;
+    private double y;
+
+    /**
+     * calls reset
+     */
+
+    public AdvancedEnemyBot()
+    {
+        reset();
+    }
+
+
+    /**
+     * returns x
+     * 
+     * @return x
+     */
+
+    public double getX()
+    {
+        return x;
+    }
+
+
+    /**
+     * returns y
+     * 
+     * @return y
+     */
+
+    public double getY()
+    {
+        return y;
+    }
+
+
+    /**
+     * updates all parameters
+     * 
+     * @param e
+     *            ScannedRobotEvent
+     * @param robot
+     *            Robot
+     */
+
+    public void update( ScannedRobotEvent e, Robot robot )
+    {
+        super.update( e );
+        double absBearingDeg = ( robot.getHeading() + e.getBearing() );
+        if ( absBearingDeg < 0 )
+        {
+            absBearingDeg += 360;
+        }
+        x = robot.getX()
+            + Math.sin( Math.toRadians( absBearingDeg ) ) * e.getDistance();
+        y = robot.getY()
+            + Math.cos( Math.toRadians( absBearingDeg ) ) * e.getDistance();
+    }
+
+
+    /**
+     * returns future x
+     * 
+     * @return future x
+     * @param when
+     *            long
+     */
+    public double getFutureX( long when )
+    {
+        return x
+            + Math.sin( Math.toRadians( getHeading() ) ) * getVelocity() * when;
+    }
+
+
+    /**
+     * returns y
+     * 
+     * @return future y
+     * @param when
+     *            long
+     */
+    public double getFutureY( long when )
+    {
+        return y
+            + Math.cos( Math.toRadians( getHeading() ) ) * getVelocity() * when;
+    }
+
+
+    /**
+     * resets
+     */
+    public void reset()
+    {
+        super.reset();
+        x = 0;
+        y = 0;
+    }
+}
